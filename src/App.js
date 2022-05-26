9fcb2157dad15e8548d63710f17f489876e7fe34
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-05-26 20:49:26 +0530

    addons

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ import './App.css';
 const App = () => (
   <BrowserRouter>
     <Switch>
-    <Route exact path="/login"  component={LoginForm}/>
-    <Route exact path="/"  component={Home}/>
-    <Route exact path="/products"  component={Products}/>
-    <Route exact path="/cart"  component={Cart}/>
-    <Route  component={NotFound}/>
-    </Switch>
+      <Route exact path="/login"  component={LoginForm}/>
+      <Route exact path="/"  component={Home}/>
+      <Route exact path="/products"  component={Products}/>
+      <Route exact path="/cart"  component={Cart}/>
+      <Route  component={NotFound}/>
+      </Switch>
   </BrowserRouter>
 )
 
