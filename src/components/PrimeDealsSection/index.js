e1ae04aa9cd3e508741bc9bf8f72ae5f2815b1fc
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-05-30 17:49:20 +0530

    addons

--- src/components/PrimeDealsSection/index.js
+++ src/components/PrimeDealsSection/index.js
@@ -1,10 +1,16 @@
 import { Component } from "react";
 
+const apiStatusConstants = {
+    initial: "INITIAL",
+    succcess: "SUCCESS",
+    failure: "FAILURE",
+    inProgress: "IN_PROGRESS",
+}
 
 class PrimeDealsSection extends Component {
     state = {
         primeDeals: [],
-        apiStatus:'',
+        apiStatus: apiStatusConstants.initial,
     }
     render() {
         return <h1>Prime Deals Section</h1>
